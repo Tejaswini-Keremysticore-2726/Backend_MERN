63a54544d9b69d1e91b1624ef8fed42080935599
tejaswinikere3113-dot <tejaswinikere3113-dot>
2026-03-06 09:59:34 +0530

    morning update

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,15 +1,3 @@
-// const express = require("express");
-// const authRoutes = require("../src/Routes/authRoute");
-// const cors = require("cors");
-
-// const app = express();
-
-// // app.use(cors());
-// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-// app.use(express.json());
-// app.use("/api", authRoutes);
-
-// module.exports = app;
 
 const express = require("express");
 const router = express.Router();
