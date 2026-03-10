1db1a725933f31e12a6b78c312185b02ce0fd614
tejaswinikere3113-dot <tejaswinikere3113-dot>
2026-03-10 10:14:41 +0530

    10 Morning Update

---
+++ Backend/src/models/productsModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const productsdetailed = new mongoose.Schema(
+  {
+    pro_name: {
+      type: String,
+      required: true,
+    },
+    pro_price: {
+      type: Number,
+      required: true,
+    },
+    pro_url: {
+      type: String,
+    },
+    pro_category: {
+      type: String,
+      required: true,
+    },
+    pro_stock: {
+      type: Number,
+    },
+  },
+
+  { timestamps: true },
+);
+module.exports = mongoose.model("product", productsdetailed);
